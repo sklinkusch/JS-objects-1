e2be5950b8253f82d95d87a14c43f4e137073ed2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-01-22 09:19:17 +0100

    brought it to original form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 function listAllMethods(obj) {
   let association;
   let MyMethodArray = Object.getOwnPropertyNames(obj);
-  let MyFilteredArray = MyMethodArray.filter(function (element) {
+  let MyFilteredArray = MyMethodArray.filter(function(element) {
     association = obj[element];
-    //    if (typeof association != "number") {
-    if (typeof association == "function") {
+    if (typeof association != "number") {
+      // if (typeof association == "function") {
       console.log(element);
     }
   });
